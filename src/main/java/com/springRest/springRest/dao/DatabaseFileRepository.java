8e978a592ebbbc5372f7fb0cb3da94c57a6938d3
MAYANK\mayank jha <mayank.jha1993>
2021-05-30 20:27:24 +0530

    Removing LOB causing Large Object Error

--- src/main/java/com/springRest/springRest/dao/DatabaseFileRepository.java
+++ src/main/java/com/springRest/springRest/dao/DatabaseFileRepository.java
@@ -1,14 +1,11 @@
 package com.springRest.springRest.dao;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springRest.springRest.entities.DatabaseFile;
-import com.springRest.springRest.payload.ImageDataResponse;
 
 @Repository
 public interface DatabaseFileRepository extends JpaRepository<DatabaseFile, String> {
